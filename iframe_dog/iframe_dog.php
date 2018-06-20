cce9cd25cd1ed874a31e05a2b282791ec164d7c8
Carseason <757841203@qq.com>
2018-06-20 12:57:43 +0800

    Add files via upload

--- iframe_dog/iframe_dog.php
+++ iframe_dog/iframe_dog.php
@@ -3,7 +3,7 @@
 Plugin Name: iframe_dog
 Plugin URI: #
 Description: 嵌套ifame视频
-Version: 1.0
+Version: 1.2
 Author: Carseason
 Author URI: https://github.com/Carseason/iframe_dog
 */
@@ -12,9 +12,6 @@ add_shortcode('dog', 'iframe_dog');
 //dog为短代码名称,iframe_dog为函数
 //即你在编辑文章时输入[dog]就会执行 iframe_dog 函数
 function iframe_dog($array_url_title) {//短代码要处理的函数
-
-    wp_enqueue_style('iframe_dog', plugins_url('iframe_dog.css', __FILE__), false, '');   //引用js
-    wp_enqueue_script('iframe_dog', plugins_url('iframe_dog.js', __FILE__), false, '');   //引用css
     $dog_url= explode(',',$array_url_title["url"]); //文章url短代码链接,以英文逗号分割
     $dog_name= explode(',',$array_url_title["name"]);//文章短代码名字,以英文逗号分割
     $dog_url_i=  count($dog_url);   //获取数量
@@ -23,16 +20,35 @@ function iframe_dog($array_url_title) {//短代码要处理的函数
     $dog_html = "";
     for(    $i=0;   $i<$dog_i;  $i++){
         global  $dog_html;
-        $dog_html .=  '<div class="d_video" onclick="iframedog(\''.$dog_url[$i].'\');" title="'.$dog_name[$i].'"><a>'.$dog_name[$i].'</a></div>';
+        if (strpos($dog_url[$i],'av=')!==false){
+              $dog_html .=  '<div class="d_video" data-video="//player.bilibili.com/player.html?aid='.(str_replace("av=","",$dog_url[$i])).'" title="'.$dog_name[$i].'">'.$dog_name[$i].'</div>';
+        }else{
+            $dog_html .=  '<div class="d_video" data-video="/wp-content/plugins/iframe_dog/Dplayer.php?'.$dog_url[$i].'" title="'.$dog_name[$i].'">'.$dog_name[$i].'</div>';
+        }    
     }
-   // echo $dog_html;
-    echo '<div class="v-player" ><div class="listSkip">'.$dog_html.'</div><div id="iframe" class="iframe" ><iframe src="'.$dog_url[0].'" frameborder="0" allowfullscreen="" scrolling="none"></iframe></div></div>';
+    $dog_video="";
+    if (strpos($dog_url[0],'av=')!==false){
+        $dog_video="//player.bilibili.com/player.html?aid=".(str_replace("av=","",$dog_url[0]));
+    }else{
+        $dog_video="/wp-content/plugins/iframe_dog/Dplayer.php?".$dog_url[0];
+    }
 
 
-  // return $array_url_title["url"].$array_url_title["name"];
 
+return <<<EOF
+<script src="/wp-content/plugins/iframe_dog/jquery-3.3.1.min.js"></script>
+<script src="/wp-content/plugins/iframe_dog/iframe_dog.js"></script>
+<link rel="stylesheet" href="/wp-content/plugins/iframe_dog/iframe_dog.css" type="text/css" media="all">
+<div class="v-player" >
+    <div id="iframe_dp" class="iframe_dp" >
+        <iframe src="$dog_video" frameborder="0" allowfullscreen="" scrolling="none">
+        </iframe>
+    </div>
+    <div class="listSkip">
+    $dog_html
+    </div>
+</div>
 
+EOF;
 }
-
-//[dog url="1,2" name="第01集,第02集" ]
 ?>
